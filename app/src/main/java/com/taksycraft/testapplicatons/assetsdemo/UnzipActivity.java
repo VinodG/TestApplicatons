fe927366047dda7feac6aeae31e2061a8ced5644
VINOD KUMAR <vinod.ponting>
2019-12-09 23:49:42 +0530

    unzip folder from asset is done

---
+++ app/src/main/java/com/taksycraft/testapplicatons/assetsdemo/UnzipActivity.java
@@ -0,0 +1,95 @@
+package com.taksycraft.testapplicatons.assetsdemo;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.taksycraft.testapplicatons.R;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class UnzipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unzip);
+        copyfromAssets();
+        if(unpackZip("/data/data/"+getPackageName()+"/zip/","vinod.zip"))
+            Toast.makeText(this, "unziped ",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void unZipFiles() {
+
+    }
+    private boolean unpackZip(String path, String zipname)
+    {
+        InputStream is;
+        ZipInputStream zis;
+        try
+        {
+            is = new FileInputStream(path + zipname);
+            zis = new ZipInputStream(new BufferedInputStream(is));
+            ZipEntry ze;
+            while((ze = zis.getNextEntry()) != null)
+            {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int count;
+
+                String filename = ze.getName();
+                FileOutputStream fout = new FileOutputStream(path + "modified");
+
+                // reading and writing
+                while((count = zis.read(buffer)) != -1)
+                {
+                    baos.write(buffer, 0, count);
+                    byte[] bytes = baos.toByteArray();
+                    fout.write(bytes);
+                    baos.reset();
+                }
+
+                fout.close();
+                zis.closeEntry();
+            }
+
+            zis.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void copyfromAssets() {
+        File f   = new File("/data/data/"+getPackageName()+"/zip/vinod.zip");
+        File dir   = new File("/data/data/"+getPackageName()+"/zip/");
+        dir.mkdir();
+        if (!f.exists()) try {
+
+            InputStream is = getAssets().open("temp.zip");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(buffer);
+            fos.close();
+        } catch (Exception e) { throw new RuntimeException(e); }
+    }
+}
